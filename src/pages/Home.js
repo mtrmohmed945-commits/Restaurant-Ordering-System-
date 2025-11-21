15fc20274ac6dbb13693e9e76064e8348e99ad38
mtrmohmed945-commits <mtrmohmed945@gmail.com>
2025-11-21 19:44:04 +0200

    Added Home page with hero section and basic styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,21 @@
+import "../styles/Home.css";
+import { Link } from "react-router-dom";
+
 export default function Home() {
-  return <h1>Home Page</h1>;
+  return (
+    <div className="home">
+
+      <div className="overlay"></div>
+
+      <div className="home-content">
+        <h1>Welcome to FoodExpress</h1>
+        <p>Your favorite meals delivered hot and fresh!</p>
+
+        <Link to="/menu" className="home-btn">
+          View Menu
+        </Link>
+      </div>
+
+    </div>
+  );
 }
