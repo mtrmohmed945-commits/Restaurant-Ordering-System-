03d2dde358695a817bfc36a5243633f8740a423a
mtrmohmed945-commits <mtrmohmed945@gmail.com>
2025-12-28 13:01:46 +0200

    Phase 2: Setup MySQL database connection and backend base

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,22 @@
+import dotenv from "dotenv";
+dotenv.config(); // MUST be first
+
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
+import db from "./db.js"; // force connection
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public"));
-app.use(morgan("dev"))
-// Test route
-app.get("/", (req, res) => {
-  res.sendFile('/index.html')
+app.use(morgan("dev"));
+
+app.get("/api", (req, res) => {
+  res.send("Backend running 🚀");
 });
 
-// Server
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
