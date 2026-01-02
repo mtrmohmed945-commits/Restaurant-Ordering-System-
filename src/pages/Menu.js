78acbc926edb2f9acb420df9bb08c33576dc7d43
mtrmohmed945-commits <mtrmohmed945@gmail.com>
2026-01-02 17:06:51 +0200

    fix menu add to cart

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -9,37 +9,39 @@ export default function Menu() {
   useEffect(() => {
     fetch("http://localhost:5000/api/menu")
       .then(res => res.json())
-      .then(data => {
-        if (Array.isArray(data)) {
-          setMenu(data);
-        } else {
-          console.error("Menu API error:", data);
-          setMenu([]);
-        }
-      });
+      .then(data => setMenu(Array.isArray(data) ? data : []));
   }, []);
 
   const addToCart = (item) => {
+    if (cart.some(i => i.id === item.id)) return;
     setCart([...cart, { ...item, quantity: 1 }]);
   };
 
+  const isInCart = (id) => cart.some(item => item.id === id);
+
   return (
-<div className="menu-grid">
-  {menu.map((item) => (
-    <div className="menu-card" key={item.id}>
-      {item.image && (
-        <img
-          src={`http://localhost:5000/uploads/${item.image}`}
-          alt={item.name}
-          className="menu-img"
-        />
-      )}
-      <h2>{item.name}</h2>
-      <p>{item.description}</p>
-      <p>${Number(item.price).toFixed(2)}</p>
-      <button onClick={() => addToCart(item)}>Add to Cart</button>
+    <div className="menu-grid">
+      {menu.map(item => (
+        <div className="menu-card" key={item.id}>
+          {item.image && (
+            <img
+              src={`http://localhost:5000/uploads/${item.image}`}
+              alt={item.name}
+              className="menu-img"
+            />
+          )}
+          <h2>{item.name}</h2>
+          <p>{item.description}</p>
+          <p>${Number(item.price).toFixed(2)}</p>
+
+          <button
+            onClick={() => addToCart(item)}
+            disabled={isInCart(item.id)}
+          >
+            {isInCart(item.id) ? "Added" : "Add to Cart"}
+          </button>
+        </div>
+      ))}
     </div>
-  ))}
-</div>
-  )
+  );
 }
